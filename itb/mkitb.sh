ecbda8f01f2a02636d16a0ad773af87c721bca61
liji <liji6630@live.com>
2018-12-06 01:05:02 -0800

    内核与文件系统打包一起，修改了一些配置

--- itb/mkitb.sh
+++ itb/mkitb.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
+cd $1/itb
 
-rm Image.gz 
+cp linux-4.9.111/arch/arm64/boot/Image ./
 gzip Image
-mkimage -f ./linux_arm64.its ./lsdk_linux_arm64_tiny.itb
+mkimage -f linux_arm64.its lsdk_linux_arm64_tiny.itb
+sudo cp lsdk_linux_arm64_tiny.itb $1/target/linux/layerscape/base-files/
