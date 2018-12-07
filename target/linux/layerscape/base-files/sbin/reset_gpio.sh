9f81b770644b658355aae951547627d17ae77e40
liji <liji6630@live.com>
2018-12-06 20:58:42 -0800

    修复了恢复配置不生效的问题

--- target/linux/layerscape/base-files/sbin/reset_gpio.sh
+++ target/linux/layerscape/base-files/sbin/reset_gpio.sh
@@ -14,7 +14,7 @@ do
 			num=$(cat /sys/class/gpio/gpio475/value)
 			if [ "$num" = "0" ];then
 				echo "reboot..........."
-				/sbin/sys_update.sh
+				mkdir /recover && mount /dev/mmcblk0p3 /recover && touch /recover/recover && umount /recover && rm -rf /recover && sync
 				reboot
 			fi
 	fi
