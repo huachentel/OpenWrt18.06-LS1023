352a81bbe8789fc82ec8b8639fe46439d7d7dabd
liji <liji6630@live.com>
2018-12-10 01:06:09 -0800

    修改了reset按键功能，修改了sysupgrade用法

--- target/linux/layerscape/base-files/sbin/reset_gpio.sh
+++ target/linux/layerscape/base-files/sbin/reset_gpio.sh
@@ -5,18 +5,22 @@ if [ ! -d "/sys/class/gpio/gpio475" ];then
 	echo in > /sys/class/gpio/gpio475/direction
 fi
 
+
 while [ "1" = "1" ]
 do
 	num=$(cat /sys/class/gpio/gpio475/value)
-	if [ "$num" = "0" ];
-		then
-			sleep 3
-			num=$(cat /sys/class/gpio/gpio475/value)
-			if [ "$num" = "0" ];then
-				echo "reboot..........."
-				mkdir /recover && mount /dev/mmcblk0p3 /recover && touch /recover/recover && umount /recover && rm -rf /recover && sync
-				reboot
-			fi
+	if [ "$num" = "0" ]; then
+		sleep 1
+		num=$(cat /sys/class/gpio/gpio475/value)
+		if [ "$num" = "0" ]; then
+		/etc/rc.button/reset
+		fi
 	fi
+	
+	while [ "$num" = "0" ]
+	do
+		num=$(cat /sys/class/gpio/gpio475/value)
+		sleep 1;
+	done
 	sleep 1;
 done
